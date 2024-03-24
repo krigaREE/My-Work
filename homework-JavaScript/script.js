4a720124a6ec6a69d63a6907d9a4687880ed7307
krigaREE <krigarepkr@gmail.com>
2024-03-24 12:55:28 +0200

    new work uploaded

---
+++ homework-JavaScript/script.js
@@ -0,0 +1,50 @@
+// // EXERCISE ONE
+// const name = prompt("Give your name");
+// alert(`Your name is ${name}`);
+
+// //EXERCISE TWO
+// const currentYear = new Date().getFullYear();
+// const yearOfBirth = prompt("What is your year of birth:");
+// const age = currentYear - yearOfBirth;
+// alert(age);
+
+// //EXERCISE THREE
+// const sideLength = prompt("Give me the length of a side of a square");
+// const perimeter = sideLength * 4;
+// alert(perimeter);
+
+// //EXERCISE FOUR
+// const Pi = 3.14;
+// const r = prompt("What is the radius of the circle:");
+// const area = Pi * Math.pow(r, 2);
+// alert(area);
+
+// //EXERCISE FIVE
+// const distanceInKm = prompt("What is the distance you want to cover between the two cities");
+// const timeInMins = prompt("What is the time you want to spend while covering the two cities");
+// const timeInHours = timeInMins / 60;
+// const averageSpeed = distanceInKm / timeInHours;
+// alert(averageSpeed + " km/h");
+
+// //EXERCISE SIX
+// const EUR_TO_USD_RATE = 0.91;
+// const enterAmount = prompt("What is the USD amount you want to convert to EUR?:");
+// const exchangeRate = enterAmount * EUR_TO_USD_RATE;
+// alert(exchangeRate);
+
+// //EXERCISE SEVEN
+// const MB_IN_GB = 1024;
+// const PARTITION_SIZE = 820;
+// const flashMemoryInGB = prompt("What is your flash drive memory in GB?:");
+// const flashMemoryInMB = flashMemoryInGB * MB_IN_GB;
+// const numOfPartitions = Math.floor(flashMemoryInMB / PARTITION_SIZE);
+// alert(numOfPartitions);
+
+// //EXERCISE EIGHT
+// const moneyInWallet = prompt("What is the amount of money in your wallet?");
+// const chocolatePrice = prompt("What is the price of one chocolate bar?");
+// const chocolateCount = Math.floor(moneyInWallet / chocolatePrice);
+// const moneyLeft = moneyInWallet - chocolateCount * chocolatePrice;
+// alert(`chocolates:${chocolateCount} left: ${moneyLeft}`)
+
+
